897ef7cae46c28025395503d78e3eda6a04a6ba0
Wagner Ghedin <wagnerghedin10@gmail.com>
2022-03-10 20:13:48 -0300

    Add files via upload

---
+++ src/compiladores.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Stack;
+
+public class compiladores {
+    public static void main(String[] args) {
+        try {
+            String path = "files/texto";
+            testSavingFile(path);
+            testReadingFile(path);
+        } catch (Exception e) {
+            System.out.println("Error while executing the program");
+            e.printStackTrace();
+        }
+    }
+
+    public static void testSavingFile(String path) throws FileNotFoundException, IOException {
+        String texto = """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Nam pretium luctus augue, et accumsan libero lobortis vitae. Aliquam non elit id diam dictum ullamcorper. Pellentesque at mi non odio iaculis elementum nec sit amet velit. Donec luctus turpis tempor lectus luctus, id posuere erat tincidunt. Pellentesque quis dictum augue. Donec vitae tellus non sapien efficitur placerat in at est. Aliquam diam odio, rhoncus vitae consectetur cursus, sollicitudin sed lacus.
+                1.025 569 1 2
+                Cras dignissim erat in imperdiet imperdiet. Donec eget ante urna. Nulla vel lectus hendrerit est placerat sollicitudin. Integer porttitor velit sed mattis dictum. Vivamus non mi id purus euismod consectetur. Suspendisse sed.""";
+
+        src.fileHandler.saveFile(path, texto);
+    }
+
+    public static void testReadingFile(String path) throws FileNotFoundException, IOException {
+        StringBuilder texto = src.fileHandler.readFile(path);
+        String meuTexto = texto.toString();
+        Stack minhaPilha = new Stack();
+
+        // System.out.println(texto.toString());
+
+        // for (int i = 0; i < meuTexto.length(); i++) {
+        //     minhaPilha.add(meuTexto.split(" "));
+        // }
+    }
+}
---
+++ src/fileHandler.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class fileHandler {
+    public static void saveFile(String path, String str) throws FileNotFoundException, IOException {
+        FileOutputStream arq = null;
+        PrintStream ps = null;
+
+        File f = new File(path);
+        arq = new FileOutputStream(f);
+
+        try {
+            try {
+                ps = new PrintStream(arq);
+                ps.println(str);
+            } finally {
+                if (ps != null) {
+                    ps.close();
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error while saving the file/n");
+            e.printStackTrace();
+        }
+    }
+
+    public static StringBuilder readFile(String path) throws FileNotFoundException, IOException {
+        StringBuilder result = new StringBuilder();
+        FileInputStream arq = null;
+
+        try {
+            File f = new File(path);
+            arq = new FileInputStream(f);
+
+            int readChar = arq.read();
+
+            while (readChar != -1) {
+                result.append((char) readChar);
+                readChar = arq.read();
+            }
+        } catch (Exception e) {
+            System.out.println("Error while reading the file/n");
+            e.printStackTrace();
+        } finally {
+            if (arq != null) {
+                arq.close();
+            }
+        }
+
+        return result;
+    }
+}
