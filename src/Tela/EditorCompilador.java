6363aca9e86b3452b49172de8abebe7191eaf725
Ventura <ventura09.10.0@gmail.com>
2022-07-01 20:05:08 -0300

    ajustes no consoles

--- src/Tela/EditorCompilador.java
+++ src/Tela/EditorCompilador.java
@@ -59,6 +59,7 @@ public class EditorCompilador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
+    // <editor-fold defaultstate="collapsed" desc="Generated
     // Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -79,29 +80,6 @@ public class EditorCompilador extends javax.swing.JFrame {
         NewDoc = new javax.swing.JButton();
         jScrollPane3 = new javax.swing.JScrollPane();
         TextConsole = new javax.swing.JTextArea();
-        linhas = new javax.swing.JPanel();
-        linha1 = new java.awt.Label();
-        linha2 = new java.awt.Label();
-        linha3 = new java.awt.Label();
-        linha4 = new java.awt.Label();
-        linha5 = new java.awt.Label();
-        linha6 = new java.awt.Label();
-        linha7 = new java.awt.Label();
-        linha8 = new java.awt.Label();
-        linha9 = new java.awt.Label();
-        linha10 = new java.awt.Label();
-        linha11 = new java.awt.Label();
-        linha12 = new java.awt.Label();
-        linha13 = new java.awt.Label();
-        linha14 = new java.awt.Label();
-        linha15 = new java.awt.Label();
-        linha16 = new java.awt.Label();
-        linha17 = new java.awt.Label();
-        linha18 = new java.awt.Label();
-        linha19 = new java.awt.Label();
-        linha20 = new java.awt.Label();
-        linha21 = new java.awt.Label();
-        linha22 = new java.awt.Label();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         Abrir = new javax.swing.JCheckBoxMenuItem();
@@ -119,32 +97,14 @@ public class EditorCompilador extends javax.swing.JFrame {
         Area_Texto.setFont(new java.awt.Font("Segoe UI Variable", 0, 14)); // NOI18N
         Area_Texto.setRows(20);
         Area_Texto.setTabSize(4);
-        Area_Texto.setText(
-                "Program testeproc1;\n Var\n   X, y, z :integer;  \n Procedure P;\n Var\n   A :integer;\n Begin\n       Readln(a);\n       If a=x then\n            z:=z+x\n       Else begin\n               Z:=z-x;\n               Call p;\n       End;\n End;\n Begin\n       Z:=0;\n       Readln(x,y);\n           If x>y then\n           Call p\n       Else\n       Z:=z+x+y;\n Writeln(z);\nEnd.");
+        // Area_Texto.setText(
+        // "Program testeproc1;\n Var\n X, y, z :integer; \n Procedure P;\n Var\n A
+        // :integer;\n Begin\n Readln(a);\n If a=x then\n z:=z+x\n Else begin\n
+        // Z:=z-x;\n Call p;\n End;\n End;\n Begin\n Z:=0;\n Readln(x,y);\n If x>y
+        // then\n Call p\n Else\n Z:=z+x+y;\n Writeln(z);\nEnd.");
         Area_Texto.setToolTipText("");
         Area_Texto.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-        Area_Texto.setText(" program testeproc1;\n" +
-                "          var X, y, z :integer;\n" +
-                " procedure P; \n" +
-                "          var A :integer;\n" +
-                "              begin\n" +
-                "                  readln(a);\n" +
-                "                  if a=x then\n" +
-                "                      z:=z+x\n" +
-                "                  else begin\n" +
-                "            Z:=z+x;\n" +
-                "            call p;\n" +
-                "               end;\n" +
-                "                           end;\n" +
-                "        begin\n" +
-                "           Z:=0;\n" +
-                "           readln(x,y);\n" +
-                "             if x>y then\n" +
-                "                 call p\n" +
-                "             else\n" +
-                "           Z:=z+x+y;\n" +
-                "         writeln(z);\n" +
-                "end.\n ");
+        Area_Texto.setText("");
         jScrollPane2.setViewportView(Area_Texto);
 
         getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 50, 450, 450));
@@ -278,211 +238,6 @@ public class EditorCompilador extends javax.swing.JFrame {
 
         getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 530, 800, 110));
 
-        linhas.setAlignmentY(0.6F);
-        linhas.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-
-        linha1.setAlignment(java.awt.Label.CENTER);
-        linha1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha1.setMaximumSize(null);
-        linha1.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha1.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha1.setText("1");
-        linhas.add(linha1);
-        linha1.getAccessibleContext().setAccessibleDescription("");
-
-        linha2.setAlignment(java.awt.Label.CENTER);
-        linha2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha2.setMaximumSize(null);
-        linha2.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha2.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha2.setText("2");
-        linhas.add(linha2);
-
-        linha3.setAlignment(java.awt.Label.CENTER);
-        linha3.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha3.setMaximumSize(null);
-        linha3.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha3.setName(""); // NOI18N
-        linha3.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha3.setText("3");
-        linhas.add(linha3);
-
-        linha4.setAlignment(java.awt.Label.CENTER);
-        linha4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha4.setMaximumSize(null);
-        linha4.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha4.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha4.setText("4");
-        linhas.add(linha4);
-
-        linha5.setAlignment(java.awt.Label.CENTER);
-        linha5.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha5.setMaximumSize(null);
-        linha5.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha5.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha5.setText("5");
-        linhas.add(linha5);
-
-        linha6.setAlignment(java.awt.Label.CENTER);
-        linha6.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha6.setMaximumSize(null);
-        linha6.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha6.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha6.setText("6");
-        linhas.add(linha6);
-
-        linha7.setAlignment(java.awt.Label.CENTER);
-        linha7.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha7.setMaximumSize(null);
-        linha7.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha7.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha7.setText("7");
-        linhas.add(linha7);
-
-        linha8.setAlignment(java.awt.Label.CENTER);
-        linha8.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha8.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha8.setMaximumSize(null);
-        linha8.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha8.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha8.setText("8");
-        linhas.add(linha8);
-
-        linha9.setAlignment(java.awt.Label.CENTER);
-        linha9.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha9.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha9.setMaximumSize(null);
-        linha9.setMinimumSize(new java.awt.Dimension(15, 24));
-        linha9.setPreferredSize(new java.awt.Dimension(15, 15));
-        linha9.setText("9");
-        linhas.add(linha9);
-
-        linha10.setAlignment(java.awt.Label.CENTER);
-        linha10.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha10.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha10.setMaximumSize(null);
-        linha10.setName(""); // NOI18N
-        linha10.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha10.setText("10");
-        linhas.add(linha10);
-
-        linha11.setAlignment(java.awt.Label.CENTER);
-        linha11.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha11.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha11.setMaximumSize(null);
-        linha11.setName(""); // NOI18N
-        linha11.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha11.setText("11");
-        linhas.add(linha11);
-
-        linha12.setAlignment(java.awt.Label.CENTER);
-        linha12.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha12.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha12.setMaximumSize(null);
-        linha12.setName(""); // NOI18N
-        linha12.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha12.setText("12");
-        linhas.add(linha12);
-
-        linha13.setAlignment(java.awt.Label.CENTER);
-        linha13.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha13.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha13.setMaximumSize(null);
-        linha13.setName(""); // NOI18N
-        linha13.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha13.setText("13");
-        linhas.add(linha13);
-
-        linha14.setAlignment(java.awt.Label.CENTER);
-        linha14.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha14.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha14.setMaximumSize(null);
-        linha14.setName(""); // NOI18N
-        linha14.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha14.setText("14");
-        linhas.add(linha14);
-
-        linha15.setAlignment(java.awt.Label.CENTER);
-        linha15.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha15.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha15.setMaximumSize(null);
-        linha15.setName(""); // NOI18N
-        linha15.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha15.setText("15");
-        linhas.add(linha15);
-
-        linha16.setAlignment(java.awt.Label.CENTER);
-        linha16.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha16.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha16.setMaximumSize(null);
-        linha16.setName(""); // NOI18N
-        linha16.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha16.setText("16");
-        linhas.add(linha16);
-
-        linha17.setAlignment(java.awt.Label.CENTER);
-        linha17.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha17.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha17.setMaximumSize(null);
-        linha17.setName(""); // NOI18N
-        linha17.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha17.setText("17");
-        linhas.add(linha17);
-
-        linha18.setAlignment(java.awt.Label.CENTER);
-        linha18.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha18.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha18.setMaximumSize(null);
-        linha18.setName(""); // NOI18N
-        linha18.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha18.setText("18");
-        linhas.add(linha18);
-
-        linha19.setAlignment(java.awt.Label.CENTER);
-        linha19.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha19.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha19.setMaximumSize(null);
-        linha19.setName(""); // NOI18N
-        linha19.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha19.setText("19");
-        linhas.add(linha19);
-
-        linha20.setAlignment(java.awt.Label.CENTER);
-        linha20.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha20.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha20.setMaximumSize(null);
-        linha20.setName(""); // NOI18N
-        linha20.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha20.setText("20");
-        linhas.add(linha20);
-
-        linha21.setAlignment(java.awt.Label.CENTER);
-        linha21.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha21.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha21.setMaximumSize(null);
-        linha21.setName(""); // NOI18N
-        linha21.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha21.setText("21");
-        linhas.add(linha21);
-
-        linha22.setAlignment(java.awt.Label.CENTER);
-        linha22.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        linha22.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        linha22.setMaximumSize(null);
-        linha22.setName(""); // NOI18N
-        linha22.setPreferredSize(new java.awt.Dimension(20, 15));
-        linha22.setText("22");
-        linhas.add(linha22);
-
-        getContentPane().add(linhas, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 20, 450));
-
         jMenu1.setText("Arquivo");
 
         Abrir.setSelected(false);
@@ -563,7 +318,7 @@ public class EditorCompilador extends javax.swing.JFrame {
             SalvarArquivo();
             JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
 
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Arquivo salvo com sucesso!\n");
         }
     }// GEN-LAST:event_SalvarActionPerformed
@@ -602,7 +357,7 @@ public class EditorCompilador extends javax.swing.JFrame {
             SalvarArquivo();
             JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
 
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Arquivo salvo com sucesso!\n");
         }
     }// GEN-LAST:event_SalvarIconActionPerformed
@@ -618,7 +373,7 @@ public class EditorCompilador extends javax.swing.JFrame {
 
             Area_Texto.setText("");
 
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Compilador clear!\n");
         }
     }// GEN-LAST:event_LimparActionPerformed
@@ -664,7 +419,7 @@ public class EditorCompilador extends javax.swing.JFrame {
             Table.updateUI();
 
             Area_Texto.setText("");
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Novo arquivo criado com sucesso!\n");
         }
     }// GEN-LAST:event_NewDocActionPerformed
@@ -699,12 +454,12 @@ public class EditorCompilador extends javax.swing.JFrame {
             fileReader.close();
             reader.close();
 
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Arquivo aberto com sucesso!\n");
 
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(null, erro.getMessage());
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Erro ao abrir o arquivo!\n");
         }
     }
@@ -714,7 +469,7 @@ public class EditorCompilador extends javax.swing.JFrame {
         String title = "Confirmação";
         int resposta = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
         if (resposta == JOptionPane.YES_OPTION) {
-            TextConsole.setForeground(Color.BLACK);
+            TextConsole.setForeground(Color.GREEN);
             TextConsole.append("Compilador encerrado\n");
             System.exit(0);
         }
@@ -727,8 +482,8 @@ public class EditorCompilador extends javax.swing.JFrame {
             escrever = new FileWriter(new File(EnderecoArquivo) + ".txt");
             escrever.write(Area_Texto.getText());
             escrever.close();
-            TextConsole.setForeground(Color.BLACK);
-            TextConsole.append("Arquivo salvo com sucesso!\n");
+            TextConsole.setForeground(Color.GREEN);
+            // TextConsole.append("Arquivo salvo com sucesso!\n");
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(null, erro.getMessage());
         }
@@ -770,29 +525,6 @@ public class EditorCompilador extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
-    private java.awt.Label linha1;
-    private java.awt.Label linha10;
-    private java.awt.Label linha11;
-    private java.awt.Label linha12;
-    private java.awt.Label linha13;
-    private java.awt.Label linha14;
-    private java.awt.Label linha15;
-    private java.awt.Label linha16;
-    private java.awt.Label linha17;
-    private java.awt.Label linha18;
-    private java.awt.Label linha19;
-    private java.awt.Label linha2;
-    private java.awt.Label linha20;
-    private java.awt.Label linha21;
-    private java.awt.Label linha22;
-    private java.awt.Label linha3;
-    private java.awt.Label linha4;
-    private java.awt.Label linha5;
-    private java.awt.Label linha6;
-    private java.awt.Label linha7;
-    private java.awt.Label linha8;
-    private java.awt.Label linha9;
-    private javax.swing.JPanel linhas;
     // End of variables declaration//GEN-END:variables
 
 }
